a8b6acb48a44593d1bf4a8dc09821a7a0f91f10b
Robert Hieger <rhieger@gmail.com>
2020-05-24 15:02:47 -0400

    Add toContain matcher to matchers.test.js

--- test/matchers.test.js
+++ test/matchers.test.js
@@ -8,3 +8,22 @@ const greeting = () => {
 test("returned message is Hello, World!", () => {
   expect(greeting()).toBe("Hello, World!");
 });
+
+// Matcher: toContain
+
+const peripherals = [
+  "2TB external SSD",
+  "6TB USB C HD portable",
+  "SD Card Reader",
+  "digitizer pad",
+  "external blu-ray drive",
+  "4K secondary display",
+  "64GB USB2.0 Flash Drive",
+  "128GB USB3 Flash Drive",
+  "wireless bluetooth headseat",
+  "wireless bluetooth speaker"
+];
+
+test("the peripherals list contains 6TB USB C HD portable", () => {
+  expect(peripherals).toContain("6TB USB C HD portable");
+});
