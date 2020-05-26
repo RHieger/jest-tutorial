f5113048398966f9e899e71c51afff18ed503ebf
Robert Hieger <rhieger@gmail.com>
2020-05-26 17:41:31 -0400

    Refactor matchers to add descriptions to each

--- test/matchers.test.js
+++ test/matchers.test.js
@@ -5,8 +5,10 @@ const greeting = () => {
   return message;
 };
 
-test("returned message is Hello, World!", () => {
-  expect(greeting()).toBe("Hello, World!");
+describe("toBe(value)", () => {
+  test("returned message is Hello, World!", () => {
+    expect(greeting()).toBe("Hello, World!");
+  });
 });
 
 // Matcher: not value
@@ -16,8 +18,10 @@ const greeting2 = () => {
   return message;
 }
 
-test("returned message is not Hello, World!", () => {
-  expect(greeting2()).not.toBe("Hello, World!");
+describe("not.toBe(value)", () => {
+  test("returned message is not Hello, World!", () => {
+    expect(greeting2()).not.toBe("Hello, World!");
+  });
 });
 
 // Matcher: toContain
@@ -52,34 +56,37 @@ const groceries = [
   "2 cannisters ground French Vanilla Coffee"
 ];
 
-test("the peripherals list contains 6TB USB C HD portable", () => {
-  expect(peripherals).toContain("6TB USB C HD portable");
+describe("toContain(element)", () => {
+  test("the grocery list contains oatmeal", () => {
+    expect(groceries).toContain("oatmeal");
+  });
 });
 
-// Matcher: not toContain
+// Matcher: not.toContain
 
-test("the peripherals list does not contain HD Web Cam", () => {
-  expect(peripherals).not.toContain("HD Web Cam");
+describe("not.toContain(element)", () => {
+  test("the peripherals list does not contain HD Web Cam", () => {
+    expect(peripherals).not.toContain("HD Web Cam");
+  });
 });
 
 // Matcher: toHaveLength
 
-test("the peripherals list has 10 elements", () => {
-  expect(peripherals).toHaveLength(10);
+describe("toHaveLength (array elements)", () => {
+  test("the peripherals list has 10 elements", () => {
+    expect(peripherals).toHaveLength(10);
+  });
 });
 
-test("groceries list has 14 elements", () => {
-  expect(groceries).toHaveLength(14);
-});
+// Matcher: not.toHaveLength
 
-// Matcher: not toHaveLength
-
-test("the peripherals list does not have 100 elements", () => {
-  expect(peripherals).not.toHaveLength(100);
-});
-
-test("groceries list does not have 3 elements", () => {
-  expect(groceries).not.toHaveLength(3);
+describe("not.toHaveLength (array elements)", () => {
+  test("the peripherals list does not have 100 elements", () => {
+    expect(peripherals).not.toHaveLength(100);
+  });
+  test("groceries list does not have 3 elements", () => {
+    expect(groceries).not.toHaveLength(3);
+  });
 });
 
 // Matcher: toBeGreaterThan
@@ -88,26 +95,34 @@ const secondsPerDay = () => {
   return 24 * 60 * 60;
 };
 
-test("seconds per day are greater than 1440", () => {
-  expect(secondsPerDay()).toBeGreaterThan(1440);
+describe("toBeGreaterThan", () => {
+  test("seconds per day are greater than 1,440", () => {
+    expect(secondsPerDay()).toBeGreaterThan(1440);
+  });
 });
 
 // Matcher not.toBeGreaterThan
 
-test("seconds per day are not greater than 86401", () => {
-  expect(secondsPerDay()).not.toBeGreaterThan(86400);
+describe("not.toBeGreaterThan", () => {
+  test("seconds per day are not greater than 86,401", () => {
+    expect(secondsPerDay()).not.toBeGreaterThan(86400);
+  });
 });
 
 // Matcher: toBeGreaterThanOrEqual
 
-test("seconds per day are greater than or equal to 86400", () => {
-  expect(secondsPerDay()).toBeGreaterThanOrEqual(86400);
+describe("toBeGreaterThanOrEqual", () => {
+  test("seconds per day are greater than or equal to 86,400", () => {
+    expect(secondsPerDay()).toBeGreaterThanOrEqual(86400);
+  });
 });
 
 // Matcher: not.toBeGreaterThanOrEqual
 
-test("seconds per day are not greater than or equal to 86401", () => {
-  expect(secondsPerDay()).not.toBeGreaterThanOrEqual(86401);
+describe("not.toBeGreaterThanOrEqual", () => {
+  test("seconds per day are not greater than or equal to 86,401", () => {
+    expect(secondsPerDay()).not.toBeGreaterThanOrEqual(86401);
+  });
 });
 
 // Matcher: toBeLessThan
@@ -116,43 +131,55 @@ const millisecondsPerYear = () => {
   return (86400 * 1000) * 365.25;
 };
 
-test("milliseconds per year are less than 50 billion", () => {
-  expect(millisecondsPerYear()).toBeLessThan(50000000000);
+describe("toBeLessThan", () => {
+  test("milliseconds per year are less than 50 billion", () => {
+    expect(millisecondsPerYear()).toBeLessThan(50000000000);
+  });
 });
 
 // Matcher: not.toBeLessThan
 
-test("milliseconds per year are not less than 30 billion", () => {
-  expect(millisecondsPerYear()).not.toBeLessThan(30000000000);
+describe("not.toBeLessThan", () => {
+  test("milliseconds per year are not less than 30 billion", () => {
+    expect(millisecondsPerYear()).not.toBeLessThan(30000000000);
+  });
 });
 
 // Matcher: toBe (number)
 
-test("milliseconds per year are 31,557,600,000", () => {
-  expect(millisecondsPerYear()).toBe(31557600000);
+describe("toBe(number)", () => {
+  test("milliseconds per year are 31,557,600,000", () => {
+    expect(millisecondsPerYear()).toBe(31557600000);
+  });
 });
 
 // Matcher not.toBe (number)
 
-test("milliseconds per year are not 32,000,000,000", () => {
-  expect(millisecondsPerYear()).not.toBe(32000000000);
+describe("not.toBe(number)", () => {
+  test("milliseconds per year are not 32,000,000,000", () => {
+    expect(millisecondsPerYear()).not.toBe(32000000000);
+  });
 });
 
 // Matcher: toBeLessThanOrEqual
 
-test("milliseconds per year are less than or equal to 31,557,601,000",
-  () => {
-    expect(millisecondsPerYear()).toBeLessThanOrEqual(31557601000);
-  });
+describe("toBeLessThanOrEqual", () => {
+  test("milliseconds per year are less than or equal to 31,557,601,000",
+    () => {
+      expect(millisecondsPerYear()).toBeLessThanOrEqual(31557601000);
+    });
+});
 
 // Matcher: not.toBeLessThanOrEqual
 
-test(`milliseconds per year are not less than
-    or equal to 31,557,599,000`, () => {
-  expect(millisecondsPerYear()).not.toBeLessThanOrEqual(31557599000);
+describe("not.toBeLessThanOrEqual", () => {
+  test(`milliseconds per year are not less than
+      or equal to 31,557,599,000`, () => {
+    expect(millisecondsPerYear()).not.toBeLessThanOrEqual(31557599000);
+  });
 });
 
-// Matcher: toContainEqual(item)
+// Matcher: toContainEqual(property)
 
 const people = [
   { name: "Joe Smith", age: 32, sex: "male" },
@@ -161,9 +188,20 @@ const people = [
   { name: "Robert Roth", age: 52, sex: "male" }
 ];
 
-test("name is Jennifer James and sex is not male", () => {
-  const myPerson = { name: "Jennifer James", age: 35, sex: "female" };
-  expect(people).toContainEqual(myPerson);
+describe("toContainEqual(property)", () => {
+  test("name is Jennifer James and sex is not male", () => {
+    const myPerson = { name: "Jennifer James", age: 35, sex: "female" };
+    expect(people).toContainEqual(myPerson);
+  });
+});
+
+// Matcher: not.toContainEqual(property)
+
+describe("not.toContainEqual(property)", () => {
+  test("my name is not Robert", () => {
+    const myPerson = { name: "Jo" };
+    expect(people).not.toContainEqual(myPerson);
+  });
 });
 
 // Matcher: toEqual
