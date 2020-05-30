1082d7b12342b5e78d07eeb754ce9a8da91accf2
Landon Schropp <schroppl@gmail.com>
2020-05-30 14:29:21 -0700

    Add tests for reverse

---
+++ test/reverse.test.js
@@ -0,0 +1,41 @@
+import { reverse } from "../source/reverse";
+
+describe("reverse", () => {
+
+  describe("when the string is empty", () => {
+
+    it("returns an empty string", () => {
+
+      expect(reverse("")).toEqual("");
+    });
+  });
+
+  describe("when the string only contains one letter", () => {
+
+    it("returns the letter unmodified", () => {
+      expect(reverse("a")).toEqual("a");
+      expect(reverse("z")).toEqual("z");
+    });
+  });
+
+  describe("when the string contains multiple characters", () => {
+
+    it("returns the characters in reverse order", () => {
+      expect(reverse("abc")).toEqual("cba");
+    });
+  });
+
+  describe("when the string contains symbols", () => {
+
+    it("returns the characters in reverse order", () => {
+      expect(reverse("$%^&")).toEqual("&^%$");
+    });
+  });
+
+  describe("when the string contains emojis", () => {
+
+    it("returns the characters in reverse order", () => {
+      expect(reverse("😎🤔😵")).toEqual("😵🤔🙂");
+    });
+  });
+});
